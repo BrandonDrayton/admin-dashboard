513e262d6346ca48c97e27a1acee47d75cda5bc7
Brandon Drayton <brandondrayton@gmail.com>
2023-01-14 22:51:39 -0500

    Navbar logic and styles, Sidebar auto close mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,61 +21,82 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      >
-        {icon}
-      </span>
+      />
+      {icon}
     </button>
   </TooltipComponent>
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const {
+    activeMenu,
+    setActiveMenu,
+    isClicked,
+    handleClick,
+    screenSize,
+    setScreenSize,
+  } = useStateContext();
   const handleActiveMenu = () => setActiveMenu(!activeMenu);
-  // Placeholder for click functionality
-  const handleClick = () => setActiveMenu(!activeMenu);
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    screenSize <= 900 ? setActiveMenu(false) : setActiveMenu(true);
+  }, [screenSize]);
   return (
-    <div className="flex justify-between p-2 md:ml-6 relative">
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
       <NavButton
         title="Menu"
         customFunc={handleActiveMenu}
         color="blue"
         icon={<AiOutlineMenu />}
       />
-      <NavButton
-        title="Cart"
-        customFunc={handleClick}
-        color="blue"
-        icon={<FiShoppingCart />}
-      />
-      <NavButton
-        title="Chat"
-        dotColor="#03c9D7"
-        customFunc={handleClick}
-        color="blue"
-        icon={<BsChatLeft />}
-      />
-      <NavButton
-        title="Notifications"
-        dotColor="#03c9D7"
-        customFunc={handleClick}
-        color="blue"
-        icon={<RiNotification3Line />}
-      />
-      <TooltipComponent content="Profile" position="BottomCenter">
-        <div
-          className="flex items-center gap-2 curse-pointer p-1 hover:bg-light-gray rounded-lg"
-          onClick={() => handleClick()}
-        >
-          <img className="rounded-full w-8 h-8" src={avatar}></img>
-          <p>
-            <span className="text-gray-400 text-14">Hi, </span>{" "}
-            <span className="text-gray-400 font-bold ml-1 text-14">
-              Brandon
-            </span>
-          </p>
-          <MdKeyboardArrowDown className="text-gray-400 text-14" />
-        </div>
-      </TooltipComponent>
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick("cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+        <NavButton
+          title="Chat"
+          dotColor="#03c9D7"
+          customFunc={() => handleClick("chat")}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          dotColor="#03c9D7"
+          customFunc={() => handleClick("notification")}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 curse-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("userProfile")}
+          >
+            <img className="rounded-full w-8 h-8" src={avatar}></img>
+            <p>
+              <span className="text-gray-400 text-14">Hi, </span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Brandon
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+      </div>
+
+      {isClicked.cart && <Cart />}
+      {isClicked.chat && <Chat />}
+      {isClicked.notification && <Notification />}
+      {isClicked.userProfile && <UserProfile />}
     </div>
   );
 };
